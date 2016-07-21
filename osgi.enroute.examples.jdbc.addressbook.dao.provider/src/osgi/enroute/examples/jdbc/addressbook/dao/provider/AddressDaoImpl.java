b187d0f6fa705b33b7253ca3eb0ecb990b418cf5
Peter Kriens <peter.kriens@aqute.biz>
2016-07-21 11:40:55 +0200

    init database inline

--- osgi.enroute.examples.jdbc.addressbook.dao.provider/src/osgi/enroute/examples/jdbc/addressbook/dao/provider/AddressDaoImpl.java
+++ osgi.enroute.examples.jdbc.addressbook.dao.provider/src/osgi/enroute/examples/jdbc/addressbook/dao/provider/AddressDaoImpl.java
@@ -31,174 +31,159 @@ import osgi.enroute.examples.jdbc.addressbook.dao.datatypes.AddressDTO;
 import osgi.enroute.examples.jdbc.addressbook.dao.participants.AddressSaveParticipant;
 import osgi.enroute.examples.jdbc.addressbook.dao.participants.AddressUpdateParticipant;
 
-@Component(name = "osgi.enroute.examples.jdbc.addressbook.personaddress.dao",
-            service=AddressDao.class,
-           configurationPid = "osgi.enroute.examples.jdbc.addressbook.dao")
+@Component(name = "osgi.enroute.examples.jdbc.addressbook.personaddress.dao", service = AddressDao.class, configurationPid = "osgi.enroute.examples.jdbc.addressbook.dao")
 public class AddressDaoImpl implements AddressDao {
 
-    private Logger logger = LoggerFactory.getLogger(AddressDaoImpl.class);
-    
-    
-    
-    @Reference
-    TransactionControl transactionControl;
-    
-    @Reference
-    Coordinator coordinator;
-    
-    @Reference(unbind="-",name="provider")
-    JDBCConnectionProvider jdbcConnectionProvider;
-        
-    @Reference(target="(name=addressSave)")
-    Participant addressSaveParticipant;
-    
-    @Reference(target="(name=addressUpdate)")
-    Participant addressUpdateParticipant;
-    
-    Connection connection;
-    
-    @Activate
-    void activate(Map<String, Object> props) {
-        connection = jdbcConnectionProvider.getResource(transactionControl);
-    }
-           
-    @Deactivate
-    void deactivate(){
-        if(connection != null ){
-            try {
-                connection.close();
-            }
-            catch (SQLException e) {
-                logger.error("Error closing connection",e);
-            }
-            connection = null;
-        }
-    }
+	private Logger logger = LoggerFactory.getLogger(AddressDaoImpl.class);
 
-       
-    @Override
-    public List<AddressDTO> select(Long personId) {
+	@Reference
+	TransactionControl transactionControl;
 
-        List<AddressDTO> personsAddresses =  transactionControl.notSupported(() -> {
+	@Reference
+	private Coordinator coordinator;
 
-            List<AddressDTO> dbResults =  new ArrayList<>();
-            
-            PreparedStatement pst = connection
-                    .prepareStatement(SQL_SELECT_ADDRESS_BY_PERSON);
-            pst.setLong(1, personId);
-            
-            ResultSet rs =   pst.executeQuery();
+	@Reference(unbind = "-", name = "provider")
+	JDBCConnectionProvider jdbcConnectionProvider;
 
-            while(rs.next()){
-                AddressDTO addressDTO = mapRecordToAddress(rs);
-                dbResults.add(addressDTO);
-            }
+	@Reference(target = "(name=addressSave)")
+	Participant addressSaveParticipant;
 
-            return dbResults;
-        });
+	@Reference(target = "(name=addressUpdate)")
+	Participant addressUpdateParticipant;
 
-        return personsAddresses;
-    }
+	Connection connection;
 
-    @Override
-    public AddressDTO findByPK(String pk) {
-      
-        AddressDTO personAddress = transactionControl.supports(() -> {
+	@Activate
+	void activate(Map<String, Object> props) throws SQLException {
+		connection = jdbcConnectionProvider.getResource(transactionControl);
+		transactionControl.supports( () -> connection.prepareStatement(PersonAddressTable.INIT).execute());
+	}
 
-            AddressDTO addressDTO = null;
-         
-            PreparedStatement pst = connection.prepareStatement(SQL_SELECT_ADDRESS_BY_PK);
-            pst.setString(1, pk);
+	@Deactivate
+	void deactivate() throws SQLException {
+		connection.close();
+	}
 
-            ResultSet rs = pst.executeQuery();
-            
-            if(rs.next()){
-                addressDTO = mapRecordToAddress(rs);
-            }
+	@Override
+	public List<AddressDTO> select(Long personId) {
 
-            return addressDTO;
-        });
+		List<AddressDTO> personsAddresses = transactionControl.notSupported(() -> {
 
-        return personAddress;
-    }
+			List<AddressDTO> dbResults = new ArrayList<>();
 
-    @Override
-    public void save(Long personId, AddressDTO data) {
-      
-        boolean isThereACoordination = coordinator.addParticipant
-                (addressSaveParticipant);
-        
-        if(isThereACoordination){
-            addAddressesToCoordinator(AddressSaveParticipant.class,data);            
-        }else {
-            transactionControl.required( () -> {
-                PreparedStatement pst = connection.prepareStatement(SQL_ADD_ADDRESS);
-                pst.setString(1, data.emailAddress);
-                pst.setLong(2, data.personId);
-                pst.setString(3, data.city);
-                pst.setString(4, data.country);
-                logger.info("Saved Person with id {}  and Address : {}",personId,data);
-                pst.executeUpdate();      
-                return null;
-            });
-        }
-        
-    }
+			PreparedStatement pst = connection.prepareStatement(SQL_SELECT_ADDRESS_BY_PERSON);
+			pst.setLong(1, personId);
 
-    @Override
-    public void update(Long personId, AddressDTO data) {
-       
-        boolean isThereACoordination = coordinator.addParticipant
-                (addressUpdateParticipant);
-        
-        if(isThereACoordination){
-            addAddressesToCoordinator(AddressUpdateParticipant.class,data);            
-        }else {
-            transactionControl.required( () -> {
-                PreparedStatement pst = connection.prepareStatement(SQL_UPDATE_ADDRESS_BY_PK_AND_PERSON_ID);
-                pst.setString(1, data.city);
-                pst.setString(2, data.country);
-                pst.setString(3, data.emailAddress);
-                pst.setLong(4, data.personId);
-                logger.info("Updated Person Address : {}",data);
-                pst.executeUpdate();  
+			ResultSet rs = pst.executeQuery();
 
-                return null;
+			while (rs.next()) {
+				AddressDTO addressDTO = mapRecordToAddress(rs);
+				dbResults.add(addressDTO);
+			}
 
-            });    
-        }
-    }
+			return dbResults;
+		});
 
-    @Override
-    public void delete(Long personId) {
+		return personsAddresses;
+	}
 
-        transactionControl.required( () -> {
-            PreparedStatement pst = connection.prepareStatement(SQL_DELETE_ALL_ADDRESS_BY_PERSON_ID);
-            pst.setLong(1, personId);
-            logger.info("Deleted Person {} Addresses",personId);
-            pst.executeUpdate();  
+	@Override
+	public AddressDTO findByPK(String pk) {
 
-            return null;
+		AddressDTO personAddress = transactionControl.supports(() -> {
 
-        });    
-    }
+			AddressDTO addressDTO = null;
 
-    @SuppressWarnings("unchecked")
-    protected void addAddressesToCoordinator(Class<?> keyClass, AddressDTO addressDTO) {
-        Coordination coordination = coordinator.peek();
-        Map<Class<?>,Object> cordVars = coordination.getVariables();
-        List<AddressDTO> addressDtos =  (List<AddressDTO>) cordVars.
-                computeIfAbsent(keyClass, k -> new ArrayList<AddressDTO>());
-        addressDtos.add(addressDTO);
-    }
-    
-    protected AddressDTO mapRecordToAddress(ResultSet rs) throws SQLException{
-        AddressDTO addressDTO = new AddressDTO();
-        addressDTO.personId  = rs.getLong(PersonAddressTable.PERSON_ID);
-        addressDTO.emailAddress  = rs.getString(PersonAddressTable.EMAIL_ADDRESS);
-        addressDTO.city  = rs.getString(PersonAddressTable.CITY);
-        addressDTO.country  = rs.getString(PersonAddressTable.COUNTRY);
-        return addressDTO;
-    }
+			PreparedStatement pst = connection.prepareStatement(SQL_SELECT_ADDRESS_BY_PK);
+			pst.setString(1, pk);
+
+			ResultSet rs = pst.executeQuery();
+
+			if (rs.next()) {
+				addressDTO = mapRecordToAddress(rs);
+			}
+
+			return addressDTO;
+		});
+
+		return personAddress;
+	}
+
+	@Override
+	public void save(Long personId, AddressDTO data) {
+
+		boolean isThereACoordination = coordinator.addParticipant(addressSaveParticipant);
+
+		if (isThereACoordination) {
+			addAddressesToCoordinator(AddressSaveParticipant.class, data);
+		} else {
+			transactionControl.required(() -> {
+				PreparedStatement pst = connection.prepareStatement(SQL_ADD_ADDRESS);
+				pst.setString(1, data.emailAddress);
+				pst.setLong(2, data.personId);
+				pst.setString(3, data.city);
+				pst.setString(4, data.country);
+				logger.info("Saved Person with id {}  and Address : {}", personId, data);
+				pst.executeUpdate();
+				return null;
+			});
+		}
+
+	}
+
+	@Override
+	public void update(Long personId, AddressDTO data) {
+
+		boolean isThereACoordination = coordinator.addParticipant(addressUpdateParticipant);
+
+		if (isThereACoordination) {
+			addAddressesToCoordinator(AddressUpdateParticipant.class, data);
+		} else {
+			transactionControl.required(() -> {
+				PreparedStatement pst = connection.prepareStatement(SQL_UPDATE_ADDRESS_BY_PK_AND_PERSON_ID);
+				pst.setString(1, data.city);
+				pst.setString(2, data.country);
+				pst.setString(3, data.emailAddress);
+				pst.setLong(4, data.personId);
+				logger.info("Updated Person Address : {}", data);
+				pst.executeUpdate();
+
+				return null;
+
+			});
+		}
+	}
+
+	@Override
+	public void delete(Long personId) {
+
+		transactionControl.required(() -> {
+			PreparedStatement pst = connection.prepareStatement(SQL_DELETE_ALL_ADDRESS_BY_PERSON_ID);
+			pst.setLong(1, personId);
+			logger.info("Deleted Person {} Addresses", personId);
+			pst.executeUpdate();
+
+			return null;
+
+		});
+	}
+
+	@SuppressWarnings("unchecked")
+	protected void addAddressesToCoordinator(Class<?> keyClass, AddressDTO addressDTO) {
+		Coordination coordination = coordinator.peek();
+		Map<Class<?>, Object> cordVars = coordination.getVariables();
+		List<AddressDTO> addressDtos = (List<AddressDTO>) cordVars.computeIfAbsent(keyClass,
+				k -> new ArrayList<AddressDTO>());
+		addressDtos.add(addressDTO);
+	}
+
+	protected AddressDTO mapRecordToAddress(ResultSet rs) throws SQLException {
+		AddressDTO addressDTO = new AddressDTO();
+		addressDTO.personId = rs.getLong(PersonAddressTable.PERSON_ID);
+		addressDTO.emailAddress = rs.getString(PersonAddressTable.EMAIL_ADDRESS);
+		addressDTO.city = rs.getString(PersonAddressTable.CITY);
+		addressDTO.country = rs.getString(PersonAddressTable.COUNTRY);
+		return addressDTO;
+	}
 
 }
--- osgi.enroute.examples.jdbc.addressbook.dao.provider/src/osgi/enroute/examples/jdbc/addressbook/dao/provider/PersonDaoImpl.java
+++ osgi.enroute.examples.jdbc.addressbook.dao.provider/src/osgi/enroute/examples/jdbc/addressbook/dao/provider/PersonDaoImpl.java
@@ -29,191 +29,190 @@ import osgi.enroute.examples.jdbc.addressbook.dao.datatypes.PersonDTO;
 /**
  * 
  */
-@Component(name = "osgi.enroute.examples.jdbc.addressbook.person.dao",
-service=PersonDao.class,
-configurationPid = "osgi.enroute.examples.jdbc.addressbook.dao")
-public class PersonDaoImpl implements PersonDao,PersonTable {
+@Component(name = "osgi.enroute.examples.jdbc.addressbook.person.dao", service = PersonDao.class, configurationPid = "osgi.enroute.examples.jdbc.addressbook.dao")
+public class PersonDaoImpl implements PersonDao, PersonTable {
 
-    private Logger logger = LoggerFactory.getLogger(PersonDaoImpl.class);
-    
-    //Some cordinator names for better understanding
-    String ADDRESS_DAO_SAVE_COORDINATOR = "osgi.enroute.examples.jdbc.addressbook.save";
-    String ADDRESS_DAO_UPDATE_COORDINATOR = "osgi.enroute.examples.jdbc.addressbook.update";
+	private Logger logger = LoggerFactory.getLogger(PersonDaoImpl.class);
 
-    @Reference
-    TransactionControl transactionControl;
+	// Some cordinator names for better understanding
+	String ADDRESS_DAO_SAVE_COORDINATOR = "osgi.enroute.examples.jdbc.addressbook.save";
+	String ADDRESS_DAO_UPDATE_COORDINATOR = "osgi.enroute.examples.jdbc.addressbook.update";
 
-    @Reference(unbind="-",name="provider")
-    JDBCConnectionProvider jdbcConnectionProvider;
+	@Reference
+	TransactionControl transactionControl;
 
+	@Reference(unbind = "-", name = "provider")
+	JDBCConnectionProvider jdbcConnectionProvider;
 
-    @Reference
-    Coordinator coordinator;
+	@Reference
+	Coordinator coordinator;
 
-    @Reference
-    AddressDao addressDao;
+	@Reference
+	AddressDao addressDao;
 
-    Connection connection;
+	Connection connection;
 
-    @Activate
-    void start(Map<String, Object> props) {
-        connection = jdbcConnectionProvider.getResource(transactionControl);
-    }
+	@Activate
+	void start(Map<String, Object> props) throws SQLException {
+		try {
+			connection = jdbcConnectionProvider.getResource(transactionControl);
+			transactionControl.supports(() -> connection.prepareStatement(PersonTable.INIT).execute());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public List<PersonDTO> select(){
+	@Override
+	public List<PersonDTO> select() {
 
-        List<PersonDTO> persons =  transactionControl.notSupported(() -> {
+		List<PersonDTO> persons = transactionControl.notSupported(() -> {
 
-            List<PersonDTO> dbResults =  new ArrayList<>();
+			List<PersonDTO> dbResults = new ArrayList<>();
 
-            ResultSet rs =   connection.createStatement().executeQuery(SQL_SELECT_ALL_PERSONS);
+			ResultSet rs = connection.createStatement().executeQuery(SQL_SELECT_ALL_PERSONS);
 
-            while(rs.next()){
-                PersonDTO personDTO = mapRecordToPerson(rs);
-                personDTO.addresses =  addressDao.select(personDTO.personId);
-                dbResults.add(personDTO);
-            }
+			while (rs.next()) {
+				PersonDTO personDTO = mapRecordToPerson(rs);
+				personDTO.addresses = addressDao.select(personDTO.personId);
+				dbResults.add(personDTO);
+			}
 
-            return dbResults;
-        });
+			return dbResults;
+		});
 
-        return persons;
-    }
+		return persons;
+	}
 
+	@Override
+	public void delete(Long primaryKey) {
 
-    @Override
-    public void delete(Long primaryKey){
+		transactionControl.required(() -> {
+			PreparedStatement pst = connection.prepareStatement(SQL_DELETE_PERSON_BY_PK);
+			pst.setLong(1, primaryKey);
+			pst.executeUpdate();
+			addressDao.delete(primaryKey);
+			logger.info("Deleted Person with ID : {}", primaryKey);
+			return null;
+		});
+	}
 
-        transactionControl.required(() -> {
-            PreparedStatement pst = connection.prepareStatement(SQL_DELETE_PERSON_BY_PK);
-            pst.setLong(1,primaryKey);
-            pst.executeUpdate();
-            addressDao.delete(primaryKey);
-            logger.info("Deleted Person with ID : {}",primaryKey);
-            return null;
-        });
-    }
+	@Override
+	public PersonDTO findByPK(Long pk) {
 
-    @Override
-    public PersonDTO findByPK(Long pk) {
+		PersonDTO person = transactionControl.supports(() -> {
 
-        PersonDTO person = transactionControl.supports(() -> {
+			PersonDTO personDTO = null;
 
-            PersonDTO personDTO = null;
+			PreparedStatement pst = connection.prepareStatement(SQL_SELECT_PERSON_BY_PK);
+			pst.setLong(1, pk);
 
-            PreparedStatement pst = connection.prepareStatement(SQL_SELECT_PERSON_BY_PK);
-            pst.setLong(1, pk);
+			ResultSet rs = pst.executeQuery();
 
-            ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				personDTO = mapRecordToPerson(rs);
+				personDTO.addresses = addressDao.select(pk);
+			}
 
-            if(rs.next()){
-                personDTO = mapRecordToPerson(rs);
-                personDTO.addresses =  addressDao.select(pk);
-            }
+			return personDTO;
+		});
 
-            return personDTO;
-        });
+		return person;
+	}
 
-        return person;
-    }
+	@Override
+	public Long save(PersonDTO data) {
 
-    @Override
-    public Long save(PersonDTO data){
+		long personPK = transactionControl.required(() -> {
 
-        long personPK =  transactionControl.required( () -> {
+			PreparedStatement pst = connection.prepareStatement(SQL_INSERT_PERSON, Statement.RETURN_GENERATED_KEYS);
 
-            PreparedStatement pst = connection.prepareStatement(SQL_INSERT_PERSON,Statement.RETURN_GENERATED_KEYS);
+			pst.setString(1, data.firstName);
+			pst.setString(2, data.lastName);
 
-            pst.setString(1, data.firstName);
-            pst.setString(2, data.lastName);           
+			pst.executeUpdate();
 
-            pst.executeUpdate();
-            
-            AtomicLong genPersonId = new AtomicLong(data.personId);
-            
-            if(genPersonId.get() <= 0){
-                ResultSet genKeys = pst.getGeneratedKeys();
+			AtomicLong genPersonId = new AtomicLong(data.personId);
 
-                if(genKeys.next()){
-                    genPersonId.set( genKeys.getLong(1));
-                }
-            }
+			if (genPersonId.get() <= 0) {
+				ResultSet genKeys = pst.getGeneratedKeys();
 
-            logger.info("Saved Person with ID : {}",genPersonId.get());
+				if (genKeys.next()) {
+					genPersonId.set(genKeys.getLong(1));
+				}
+			}
 
-            if(genPersonId.get() > 0){
-                List<AddressDTO> addresses = data.addresses;
+			logger.info("Saved Person with ID : {}", genPersonId.get());
 
-                //START - COORDINATION
-                coordinator.begin(ADDRESS_DAO_SAVE_COORDINATOR, TimeUnit.MINUTES.toMillis(5));
+			if (genPersonId.get() > 0) {
+				List<AddressDTO> addresses = data.addresses;
 
-                addresses.stream().forEach( address ->{
-                    address.personId = genPersonId.get();
-                    addressDao.save(genPersonId.get(),address);
-                });
+				// START - COORDINATION
+				coordinator.begin(ADDRESS_DAO_SAVE_COORDINATOR, TimeUnit.MINUTES.toMillis(5));
 
-                coordinator.peek().end();
+				addresses.stream().forEach(address -> {
+					address.personId = genPersonId.get();
+					addressDao.save(genPersonId.get(), address);
+				});
 
-                //END - COORDINATION
-            }
+				coordinator.peek().end();
 
-            return genPersonId.get();
-        });           
+				// END - COORDINATION
+			}
 
-        return personPK;
-    }
+			return genPersonId.get();
+		});
 
-    @Override
-    public void update(PersonDTO data){
+		return personPK;
+	}
 
-        transactionControl.required( () -> {
+	@Override
+	public void update(PersonDTO data) {
 
-            PreparedStatement pst = connection.prepareStatement(SQL_UPDATE_PERSON_BY_PK);
-            pst.setString(1, data.firstName);
-            pst.setString(2, data.lastName);
-            pst.setLong(3, data.personId);
-            pst.executeUpdate();
+		transactionControl.required(() -> {
 
-            logger.info("Updated person : {}",data);
+			PreparedStatement pst = connection.prepareStatement(SQL_UPDATE_PERSON_BY_PK);
+			pst.setString(1, data.firstName);
+			pst.setString(2, data.lastName);
+			pst.setLong(3, data.personId);
+			pst.executeUpdate();
 
-            final long personId = data.personId;
+			logger.info("Updated person : {}", data);
 
-            List<AddressDTO> addresses = data.addresses;
+			final long personId = data.personId;
 
-            //START - COORDINATION
+			List<AddressDTO> addresses = data.addresses;
 
-            coordinator.begin(ADDRESS_DAO_UPDATE_COORDINATOR, TimeUnit.MINUTES.toMillis(5));
+			// START - COORDINATION
 
-            addresses.stream().forEach( address -> addressDao.update(personId,address));
+			coordinator.begin(ADDRESS_DAO_UPDATE_COORDINATOR, TimeUnit.MINUTES.toMillis(5));
 
-            coordinator.peek().end();
+			addresses.stream().forEach(address -> addressDao.update(personId, address));
 
-            //END - COORDINATION
-            return null;
-        });      
-    }
+			coordinator.peek().end();
 
+			// END - COORDINATION
+			return null;
+		});
+	}
 
-    protected PersonDTO mapRecordToPerson(ResultSet rs) throws SQLException {
-        PersonDTO personDTO = new PersonDTO();
-        personDTO.personId  = rs.getLong(PERSON_ID);
-        personDTO.firstName  = rs.getString(FIRST_NAME);
-        personDTO.lastName  = rs.getString(LAST_NAME);
-        return personDTO;
-    }
+	protected PersonDTO mapRecordToPerson(ResultSet rs) throws SQLException {
+		PersonDTO personDTO = new PersonDTO();
+		personDTO.personId = rs.getLong(PERSON_ID);
+		personDTO.firstName = rs.getString(FIRST_NAME);
+		personDTO.lastName = rs.getString(LAST_NAME);
+		return personDTO;
+	}
 
-    @Deactivate
-    void stop(){
-        if(connection != null ){
-            try {
-                connection.close();
-            }
-            catch (SQLException e) {
-                logger.error("Error closing connection",e);
-            }
-            connection = null;
-        }
-    }
+	@Deactivate
+	void stop() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				logger.error("Error closing connection", e);
+			}
+			connection = null;
+		}
+	}
 
 }
